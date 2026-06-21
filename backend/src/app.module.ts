2947d50bac5b13f0e35faf92b57ed29a403a310d
guihhramos <guiramos902@gmail.com>
2026-06-21 01:48:44 -0300

    Atualizando...

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
@@ -10,12 +8,6 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    // 1. Configuração para servir o React
-    ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), '..', 'frontend', 'dist'),
-      exclude: ['/api*'], // Protege as rotas do backend
-    }),
-    
     PrismaModule,
     AuthModule,
     CategoriesModule,
