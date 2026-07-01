3e0e09f306847eecd83ac46df0b1bfdd369eddad
Guilherme Silva Ramos <160366356+guihhramos@users.noreply.github.com>
2026-07-01 18:11:43 -0300

    Atualizar o api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -11,7 +11,7 @@ export const api = axios.create({
   // Em produção, como estão no mesmo servidor, usa apenas '/api'.
   baseURL: isDevelopment ? 'http://localhost:3000/api' : import.meta.env.VITE_API_URL,
 });
-// Mantemos o seu interceptor intacto para o token
+// Mantive o interceptor intacto para o token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('@FinanceManager:token');
   
